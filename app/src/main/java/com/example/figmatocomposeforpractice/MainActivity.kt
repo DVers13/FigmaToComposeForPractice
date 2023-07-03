4f50a13c41c6ce0fdceec0035d754b91db78774c
DVers13 <91010704+DVers13@users.noreply.github.com>
2023-07-03 23:32:50 +0600

    button_video_markup_statusbar_other

--- app/src/main/java/com/example/figmatocomposeforpractice/MainActivity.kt
+++ app/src/main/java/com/example/figmatocomposeforpractice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.figmatocomposeforpractice
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
@@ -8,38 +9,40 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.lazy.LazyRow
-//import androidx.compose.foundation.layout.Spacer
-//import androidx.compose.foundation.layout.height
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.foundation.layout.width
-//import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-//import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import com.example.figmatocomposeforpractice.R
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setStatusBarColor(
+                    color = Color.Transparent,
+                    darkIcons = false
+                )
+            }
             MessageCard()
         }
     }
@@ -61,8 +64,9 @@ fun MessageCard() {
         item {
             Text(
                 "Review & Ratings", modifier = Modifier
-                    .size(137.dp, 30.dp)
-                    .padding(start = 24.dp, top = 12.dp), color = Color(0xFFEEF2FB)
+                    .size(200.dp, 30.dp)
+                    .padding(start = 24.dp, top = 6.dp), color = Color(0xFFEEF2FB),
+                fontSize = 16.sp
             )
         }
         item { ReviewRatingsicon() }
@@ -110,7 +114,6 @@ fun Logo() {
         Column(modifier = Modifier.padding(bottom = 40.dp)) {
             Text(
                 "DoTA 2",
-                modifier = Modifier.padding(bottom = 10.dp),
                 color = Color.White,
                 fontSize = 26.sp
             )
@@ -157,6 +160,7 @@ fun Logo() {
 
 @Composable
 fun Marks() {
+    val contextForToast = LocalContext.current.applicationContext
     Row(
         modifier = Modifier
             .height(400.dp)
@@ -166,32 +170,42 @@ fun Marks() {
         horizontalArrangement = Arrangement.Start
 
     ) {
-        Button(onClick = {}, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF44A9F4).copy(alpha = 0.24f)), modifier = Modifier
-            .size(53.dp, 15.dp),
-            contentPadding = PaddingValues(0.dp))
+        Button(onClick = {
+            Toast.makeText(contextForToast, "Move to MOBA!", Toast.LENGTH_SHORT).show()
+        }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF44A9F4).copy(alpha = 0.24f)), modifier = Modifier
+            .size(53.dp, 18.dp),
+            contentPadding = PaddingValues(0.dp),
+            shape = RoundedCornerShape(20.dp))
         {
-            Text("MOBA", fontSize = 5.sp,
+            Text("MOBA", fontSize = 7.sp,
             color = Color(0xFF41A0E7),
                 modifier = Modifier
             )
         }
         Button(
-            onClick = {}, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF44A9F4).copy(alpha = 0.24f)),
+            onClick = {
+                Toast.makeText(contextForToast, "Move to MULTIPLAYER!", Toast.LENGTH_SHORT).show()
+            }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF44A9F4).copy(alpha = 0.24f)),
             modifier = Modifier
-                .size(93.dp, 15.dp)
+                .size(93.dp, 18.dp)
                 .padding(start = 5.dp),
-            contentPadding = PaddingValues(0.dp)
+            contentPadding = PaddingValues(0.dp),
+            shape = RoundedCornerShape(20.dp)
         )
         {
-            Text("MULTIPLAYER", fontSize = 5.sp,
+            Text("MULTIPLAYER", fontSize = 7.sp,
                 color = Color(0xFF41A0E7))
         }
-        Button(onClick = {}, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF44A9F4).copy(alpha = 0.24f)), modifier = Modifier
-            .size(73.dp, 15.dp)
+        Button(onClick = {
+            Toast.makeText(contextForToast, "Move to STRATEGY!", Toast.LENGTH_SHORT).show()
+
+        }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF44A9F4).copy(alpha = 0.24f)), modifier = Modifier
+            .size(73.dp, 18.dp)
             .padding(start = 5.dp),
-            contentPadding = PaddingValues(0.dp))
+            contentPadding = PaddingValues(0.dp),
+            shape = RoundedCornerShape(20.dp))
         {
-            Text("STRATEGY", fontSize = 5.sp,
+            Text("STRATEGY", fontSize = 7.sp,
                 color = Color(0xFF41A0E7))
         }
     }
@@ -213,36 +227,56 @@ fun Description() {
 
 @Composable
 fun PreviewDota() {
+    val contextForToast = LocalContext.current.applicationContext
     LazyRow(
         content = {
             item {
-                Image(
-                    painter = painterResource(id = R.drawable.example1),
-                    contentDescription = "example1",
-                    contentScale = ContentScale.FillBounds,
-                    modifier = Modifier
-                        .height(125.dp)
-                        .width(275.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                        .padding(horizontal = 25.dp)
-                )
+                Box(modifier = Modifier.padding(start = 10.dp, end = 5.dp).size(240.dp, 120.dp)){
+                    Image(
+                        painter = painterResource(id = R.drawable.example1),
+                        contentDescription = "example1",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .height(125.dp)
+                            .width(275.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.play),
+                        contentDescription = "play",
+                        modifier = Modifier
+                            .size(50.dp)
+                            .align(Alignment.Center)
+                            .clickable { Toast.makeText(contextForToast, "Video. Enjoy the show!", Toast.LENGTH_SHORT).show() }
+                    )
+                }
             }
             item {
-                Image(
-                    painter = painterResource(id = R.drawable.example2),
-                    contentDescription = "example2",
-                    contentScale = ContentScale.FillBounds,
-                    modifier = Modifier
-                        .height(125.dp)
-                        .width(275.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                        .padding(horizontal = 25.dp)
-                )
+                Box(modifier = Modifier.padding(start = 5.dp, end = 10.dp).size(240.dp, 120.dp)){
+                    Image(
+                        painter = painterResource(id = R.drawable.example2),
+                        contentDescription = "example2",
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier
+                            .height(125.dp)
+                            .width(275.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .clickable { Toast.makeText(contextForToast, "Another video =)", Toast.LENGTH_SHORT).show() }
+
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.play),
+                        contentDescription = "play",
+                        modifier = Modifier
+                            .size(50.dp)
+                            .align(Alignment.Center)
+                    )
+                }
             }
         }, modifier = Modifier
             .height(128.dp)
             .fillMaxWidth()
-            .padding(start = 12.dp, end = 12.dp)
+            .padding(start = 8.dp, end = 8.dp)
     )
 }
 
@@ -257,8 +291,8 @@ fun ReviewRatingsicon() {
 
             Box(
                 modifier = Modifier
-                    .size(90.dp, 60.dp)
-                    .padding(top = 10.dp)
+                    .size(80.dp, 60.dp)
+                    .padding(top = 15.dp)
             ){
                 Row(
                     verticalAlignment = Alignment.CenterVertically
@@ -267,31 +301,31 @@ fun ReviewRatingsicon() {
                         painter = painterResource(id = R.drawable.star),
                         contentDescription = "star",
                         modifier = Modifier
-                            .size(15.dp)
+                            .size(14.dp)
                     )
                     Image(
                         painter = painterResource(id = R.drawable.star),
                         contentDescription = "star",
                         modifier = Modifier
-                            .size(15.dp)
+                            .size(14.dp)
                     )
                     Image(
                         painter = painterResource(id = R.drawable.star),
                         contentDescription = "star",
                         modifier = Modifier
-                            .size(15.dp)
+                            .size(14.dp)
                     )
                     Image(
                         painter = painterResource(id = R.drawable.star),
                         contentDescription = "star",
                         modifier = Modifier
-                            .size(15.dp)
+                            .size(14.dp)
                     )
                     Image(
                         painter = painterResource(id = R.drawable.star),
                         contentDescription = "star",
                         modifier = Modifier
-                            .size(10.dp)
+                            .size(9.dp)
                     )}
             }
         }
@@ -318,8 +352,8 @@ fun Com() {
                     .clip(RoundedCornerShape(16.dp))
             )
             Column() {
-                Text("Auguste Conte", color = Color.White)
-                Text("February 14, 2019", color = Color(0xFFFFFFFF))
+                Text("Auguste Conte", color = Color.White, modifier = Modifier.padding(start = 10.dp), fontSize = 16.sp)
+                Text("February 14, 2019", color = Color(0xFFFFFFFF).copy(alpha = 0.4f), modifier = Modifier.padding(start = 10.dp), fontSize = 12.sp)
 
             }
         }
@@ -327,7 +361,9 @@ fun Com() {
         color = Color(0xFFA8ADB7),
             modifier = Modifier.padding(start = 24.dp, end = 24.dp)
         )
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+        Box(modifier = Modifier.fillMaxWidth().height(2.dp).padding(start = 35.dp, end = 35.dp).background(color = Color(0xFF1A1F29)))
+        Spacer(modifier = Modifier.height(10.dp))
         Row(
             modifier = Modifier
                 .height(58.dp)
@@ -344,8 +380,8 @@ fun Com() {
                     .clip(RoundedCornerShape(16.dp))
             )
             Column() {
-                Text("Jang Marcelino", color = Color.White)
-                Text("February 14, 2019", color = Color(0xFFFFFFFF))
+                Text("Jang Marcelino", color = Color.White, modifier = Modifier.padding(start = 10.dp), fontSize = 16.sp)
+                Text("February 14, 2019", color = Color(0xFFFFFFFF).copy(alpha = 0.4f), modifier = Modifier.padding(start = 10.dp), fontSize = 12.sp)
 
             }
         }
@@ -357,12 +393,17 @@ fun Com() {
 
 @Composable
 fun InstallButton() {
-    Button(onClick = {}, modifier = Modifier
+    val contextForToast = LocalContext.current.applicationContext
+    Button(onClick = {
+        Toast.makeText(contextForToast, "Installation, please wait ;)!", Toast.LENGTH_SHORT).show()
+
+    }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF4D144)), modifier = Modifier
         .height(64.dp)
         .fillMaxWidth()
-        .padding(start = 24.dp, end = 24.dp, bottom = 6.dp))
+        .padding(start = 24.dp, end = 24.dp, bottom = 6.dp),
+        shape = RoundedCornerShape(20.dp))
     {
-        Text("Install")
+        Text("Install", color = Color(0xFF050B18), fontSize = 20.sp)
     }
 }
 
